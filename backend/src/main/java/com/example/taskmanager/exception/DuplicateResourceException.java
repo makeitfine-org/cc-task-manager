f90804b743ce07ba4d598671c06e59466fde57e7
West One <stingion@gmail.com>
2026-03-16 15:27:59 +0100

    Remvoe files

--- backend/src/main/java/com/example/taskmanager/exception/DuplicateResourceException.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskmanager.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/example/taskmanager/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.taskmanager.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String resource, Long id) {
-        super(resource + " not found with id: " + id);
-    }
-}
